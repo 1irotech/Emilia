f9f0b573728304a62a29a771d241577334a2bbe7
1irotech <irotech015@gmail.com>
2024-12-19 20:36:27 +0530

    Update config.py

--- Emilia/config.py
+++ Emilia/config.py
@@ -8,22 +8,22 @@ def get_user_list(config, key):
 
 
 class Config(object):
-    API_HASH = "45aabfac" # API_HASH from my.telegram.org
-    API_ID = 62 # API_ID from my.telegram.org
+    API_HASH = "cb5d60ad1402ce922d53bc500abd20df" # API_HASH from my.telegram.org
+    API_ID = 22169867 # API_ID from my.telegram.org
 
-    BOT_ID = 521 # BOT_ID
+    BOT_ID = 7575301138 # BOT_ID
     BOT_USERNAME = "irowriterobot" # BOT_USERNAME
 
-    MONGO_DB_URL = "mongodb://username:password@localhost:27017/emi?directConnection=true&authSource=admin" # MongoDB URL from MongoDB Atlas
+    MONGO_DB_URL = "mongodb+srv://vikashgup87:EDRIe3bdEq85Pdpl@cluster0.pvoygcu.mongodb.net/?retryWrites=true&w=majority" # MongoDB URL from MongoDB Atlas
 
     SUPPORT_CHAT = "ironmanhindigming" # Support Chat Username
-    UPDATE_CHANNEL = "ironmanhindigaming11" # Update Channel Username
+    UPDATE_CHANNEL = "irotechbotz0" # Update Channel Username
     START_PIC = "https://iili.io/2wrEKlt.jpg" # Start Image
-    DEV_USERS = [6040984893, 6461051572, 7107018652] # Dev Users
+    DEV_USERS = [7086360370] # Dev Users
     TOKEN = "7575301138:AAGxh6m_zmkMVF68WVOwvoEjrDSpyDon2bk" # Bot Token from @BotFather
 
-    EVENT_LOGS = -10093 # Event Logs Chat ID
-    OWNER_ID = 6040984893 # Owner ID
+    EVENT_LOGS = -1002471941785 # Event Logs Chat ID
+    OWNER_ID = 7086360370 # Owner ID
  
     TEMP_DOWNLOAD_DIRECTORY = "./" # Temporary Download Directory
     BOT_NAME = "IRO WRITER" # Bot Name
